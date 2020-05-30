a50a8941f5bea38c21541664fe580a2cf812411c
Niels1987 <65661272+Niels1987@users.noreply.github.com>
2020-05-30 06:09:13 +0200

    Update CalcDateDiff.java

--- src/gui/CalcDateDiff.java
+++ src/gui/CalcDateDiff.java
@@ -11,11 +11,11 @@ public class CalcDateDiff {
 	// method to calculate the difference between given date and actual date as days count
 	public static int date (String dbDate) {
 		
-		System.out.println("übergebenes Datum: " + dbDate);
+		//System.out.println("übergebenes Datum: " + dbDate);
 		DateFormat df = new SimpleDateFormat("dd.MM.yyyy");		// set date format
 		Date dateobj = new Date();
 		String bla = df.format(dateobj);
-		System.out.println("aktuelles Datum: " + bla);
+		//System.out.println("aktuelles Datum: " + bla);
 		
 		Date date1 = null;
 		Date date2 = null;
@@ -24,9 +24,9 @@ public class CalcDateDiff {
 		// convert date strings to date format
 		try {
 			date1 = df.parse(dbDate);
-			System.out.println("übergebenes Datum als Date: " + date1);
+			//System.out.println("übergebenes Datum als Date: " + date1);
 			date2 = df.parse(bla);
-			System.out.println("aktuelles Datum als Date: " + date2);
+			//System.out.println("aktuelles Datum als Date: " + date2);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,7 +35,7 @@ public class CalcDateDiff {
 		
 		// calculate difference in milliseconds and convert it to days
 		long diff = date2.getTime() - date1.getTime();
-		System.out.println(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+		//System.out.println(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
 		int x = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 		
 		return x;
